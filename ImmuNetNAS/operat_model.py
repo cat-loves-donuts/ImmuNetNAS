153dc2fb6b0c212dd888cbe629b379980d7b55fa
cat-loves-donuts <43735308+cat-loves-donuts@users.noreply.github.com>
2020-01-13 10:39:47 +0800

    Update operat_model.py

--- ImmuNetNAS/operat_model.py
+++ ImmuNetNAS/operat_model.py
@@ -38,7 +38,7 @@ def train_model(model, linear, input_CNN, learning_rate, nsamples, load_train_da
             loss = criterion(out, target)
             print_loss = loss.data.item()
 
-            # 反向传播，更新参数
+            # update the parameters
             optimizer_module.zero_grad()
             optimizer_linear.zero_grad()
             optimizer_input_CNN.zero_grad()
@@ -73,25 +73,25 @@ def test_model(model, linear, input_CNN, load_test_dataset,test_dataset, nsample
     #     if i > nsamples:
     #         break
     #     else:
-        # 获得img(手写图片)，label标签（手写图片对应数字）
+        # get images, labels
         out = input_CNN(data)
         out = model(out)
         out = linear(out)
         loss = criterion(out, target)
         print_loss = loss.data.item()
 
-            # 损失函数乘标签大小累计
+            # loss funtion calculation
         eval_loss += loss.data.item() * target.size(0)
-            # 在10维数据中，获得最大的预测值（即预测数）
+            # get the biggest predicting result in 10 classes
         _, pred = torch.max(out, 1)
         #print("Predict result:",pred,"the target result:",target)
-            # 判断是否与真实结果相同
+            # does the prediction = the lable
         num_correct = (pred == target).sum()
 
-            # 累计真实结果
+            # get the amount of right predicting
         eval_acc += num_correct.item()
 
-        # 输出评估结果
+        # print the results
     print('\nTest Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / (len(test_dataset)), eval_acc / (len(test_dataset))))
     return eval_acc / (len(test_dataset))
 
